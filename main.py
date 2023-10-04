3eb3ba8a9760e6ff062d72ae5e4088e1705a8edf
inyour8bitdreamz18 <WaginElla2019>
2023-10-04 16:09:40 +0200

    Exercise 4

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print("Hello world from Anna P.")
 import requests
+
+print("Hello world from Anna P.")
+
+'''From presentation
 response = requests.get("https://playground.learnqa.ru/api/hello")
-print(response.text)
+print(response.text)'''
+
+#for 4th task
+response = requests.get('https://playground.learnqa.ru/api/get_text')
+print("Results:", response.text)
