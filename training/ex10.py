ea93d59e9ae470badaef1e6dd8595a0fd602124f
inyour8bitdreamz18 <WaginElla2019>
2023-11-19 17:30:05 +0100

    exercise 9

---
+++ training/ex10.py
@@ -0,0 +1,9 @@
+#python -m pytest -s training/ex10.py
+
+class TestNumberOfChars():
+    def test_number_of_chars(self):
+        phrase = input("Set a phrase: ")
+        expected_result = 15
+
+        assert len(phrase) < expected_result, f"The number of characters is more, than {expected_result}"
+
