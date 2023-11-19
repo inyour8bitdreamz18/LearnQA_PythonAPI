953ae183ad662bb13054f715b4fce74bc8b72c6a
inyour8bitdreamz18 <WaginElla2019>
2023-11-19 19:24:08 +0100

    exercise 11

--- training/ex11.py
+++ training/ex11.py
@@ -4,9 +4,10 @@ import requests
 class TestCheckCookieName:
     def test_check_cookie_name(self):
         url = "https://playground.learnqa.ru/api/homework_cookie"
-        expected_name = "HomeWork"
         response = requests.get(url=url)
         cookies = dict(response.cookies)
+        print(cookies)
         cookie_name = tuple(cookies.keys())[0]
+        expected_name = "HomeWork"
 
         assert cookie_name == expected_name
